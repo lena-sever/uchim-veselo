3b2abdeeed402ab3eb7a619e7ba4c49cae52757e
barbara <barbara165@mail.ru>
2022-03-24 10:34:17 +0300

    creating DB Models

---
+++ app/Models/Test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tests';
+
+	public static $availableFields = [
+		'id',
+        'title',
+        'description',
+        'questions',
+        'created_at'
+	];
+
+	protected $fillable = [
+        'title',
+        'description',
+        'questions',
+	];
+
+}
