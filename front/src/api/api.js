995c9cdd84c8181124d14fbacfedf1bf13a7025f
DiskoCisko <pollandjon@gmail.com>
2022-03-27 21:02:55 +0300

    Добавил запрос на получения списка курсов
    Добавил get запрос по uchim-veselo.ru/api/courses
    Сделал рефактор для thank: getCourses. Теперь диспачим данные если в ответе есть data
    Подправил компонент CoursesItem, чтобы отрисовывались картинки

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -4,7 +4,6 @@ import {urlCourses} from "../constants/url";
 
 export const coursesAPI = {
     getCourses() {
-        debugger;
         return axios.get(urlCourses)
         .then((response) => response)
         .catch((err) => err);
