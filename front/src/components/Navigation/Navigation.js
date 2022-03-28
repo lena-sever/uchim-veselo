ee217c93b51d65459dd6de7e7640f114d65afb03
Mark-vas <vasyaev.mark@mail.ru>
2022-03-28 17:47:37 +0300

    Дополнил переход на компоненту Contacts

--- front/src/components/Navigation/Navigation.js
+++ front/src/components/Navigation/Navigation.js
@@ -2,21 +2,21 @@ import { NavLink } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation() {
+    return (
+        <>
+            <NavLink to="/" className="home">
+                Учим-весело
+            </NavLink>
 
-  return (
-      <>
-          <NavLink to="/" className="home">
-              Учим-весело
-          </NavLink>
-
-          <NavLink to="/courses" className="home">
-              Courses
-          </NavLink>
-          <a href="#" class="home">
-              <div>Contacts</div>
-          </a>
-      </>
-  );
+            <NavLink to="/courses" className="home">
+                Courses
+            </NavLink>
+            {/* Реализиовал переход на страницу "Contacts" */}
+            <NavLink to="/contacts" className="home">
+                Contacts
+            </NavLink>
+        </>
+    );
 }
 
 export default Navigation;
