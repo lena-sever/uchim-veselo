cc26360904d0767ea14f466e37e08ed4418b7afb
Nina <frontend-developer@inbox.ru>
2022-03-30 19:07:06 +0300

    Начала делать компонент Lessons

--- front/src/store/courses/actions.js
+++ front/src/store/courses/actions.js
@@ -1,35 +1,34 @@
-import {coursesAPI} from '../../api/api'
+import { coursesAPI } from "../../api/api";
 
 export const COURSES_LOADING = "COURSES::COURSES_LOADING";
 export const COURSES_FAILURE = "COURSES::COURSES_FAILURE";
 export const COURSES_SUCCESS = "COURSES::COURSES_SUCCESS";
 
 export const getCoursesLoading = () => ( {
-  type: COURSES_LOADING
+    type: COURSES_LOADING
 } );
 
 export const getCoursesFailure = (err) => ( {
-  type: COURSES_FAILURE,
-  payload: err
+    type: COURSES_FAILURE,
+    payload: err
 } );
 
 export const getCoursesSuccess = (courses) => ( {
-  type: COURSES_SUCCESS,
-  payload: courses
+    type: COURSES_SUCCESS,
+    payload: courses
 } );
 
 export const getCourses = () => async(dispatch) => {
-  dispatch( getCoursesLoading() );
-  try {
-    const response = await coursesAPI.getCourses();
-    if( !response.data  ) {
-      throw new Error( "Some mistake has occurred. We are already working on it" );
+    dispatch( getCoursesLoading() );
+    try {
+        const response = await coursesAPI.getCourses();
+
+        if( !response.data ) {
+            throw new Error( "Some mistake has occurred. We are already working on it" );
+        }
+        dispatch( getCoursesSuccess( response.data ) );
+    } catch( err ) {
+        console.log( err );
+        dispatch( getCoursesFailure( err.message ) );
     }
-    // const result = await response.text();
-    // console.log( result );
-    dispatch(getCoursesSuccess(response.data));
-  } catch( err ) {
-    console.log( err );
-    dispatch( getCoursesFailure( err.message ) );
-  }
 };
