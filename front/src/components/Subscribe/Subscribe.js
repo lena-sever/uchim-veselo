5ad671e40c70f96630929f8efbe059edd7d52736
Yaroslav777999 <88874725+Yaroslav777999@users.noreply.github.com>
2022-04-06 19:13:32 +0400

    исправление

--- front/src/components/Subscribe/Subscribe.js
+++ front/src/components/Subscribe/Subscribe.js
@@ -4,6 +4,7 @@ function Subscribe() {
       <section class="design-wrp">
                 <div class="design">
                     <div class="design-title">Подпишитесь, чтобы быть в курсе изменений</div>
+                    {/* <img src="../img/beard.png"/> */}
                     <form action="#" method="GET" class="design__form">
                         <input type="email" name="email" class="design__form-input" placeholder="Email" required></input>
                         <button type="submit" class="design__form-btn">ПОДПИСАТЬСЯ</button>
