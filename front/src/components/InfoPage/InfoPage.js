c34dac1fec5582f096ad9680e56d5e7eff90411c
Yaroslav777999 <88874725+Yaroslav777999@users.noreply.github.com>
2022-04-22 10:49:25 +0400

    создал адаптив ко всем страницам под все разрешения

--- front/src/components/InfoPage/InfoPage.js
+++ front/src/components/InfoPage/InfoPage.js
@@ -3,6 +3,7 @@ import Subscribe from "../Subscribe/Subscribe";
 import "./InfoPage.css";
 import { Link } from "react-router-dom";
 import ReviewsContainer from "../Reviews/ReviewsContainer";
+import bg2 from "../InfoPage/img/bg3.png";
 
 const InfoPage = () => {
     return (
@@ -12,9 +13,11 @@ const InfoPage = () => {
                 <div class="header-title__react subtitle">
                     Образовательная платформа для детей
                 </div>
+                 
                 <Link className="try-btn" to="/courses">
                     Учиться
                 </Link>
+                <img className="header_img" src={bg2}></img>
             </div>
             <ReviewsContainer />
             <Subscribe />
