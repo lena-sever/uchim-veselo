2023597caa979d6c36d5524c7e2a366414dd89d3
Nina <frontend-developer@inbox.ru>
2022-05-26 11:30:55 +0300

    Добавление верхнего padding у отзывов

--- front/src/store/history/historySelector.js
+++ front/src/store/history/historySelector.js
@@ -1,4 +1,5 @@
 export const selectFirstHistory = state => state.historyReducer.firstPathHistory;
+// export const selectSecondHistory = state => state.historyReducer.secondPathHistory;
 export const selectLastHistory = state => state.historyReducer.lastPathHistort;
 
 export const selectErr = state => state.historyReducer.err;
